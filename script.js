428b346afa1d33ad8d06c98d32b642fb254ee9d7
João Henrique <104601194+Joao-Henriqueb@users.noreply.github.com>
2022-09-10 19:30:23 -0300

    terminando js

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ let imgatual=document.querySelector(".imgatual")
 let iconmenu=document.querySelector(".iconmenu")
 let listamenu=document.querySelector(".listamenu")
 let closee=document.querySelector(".close")
+let paiarow=document.querySelector(".arrow")
 let arrowbaack=document.querySelector(".back")
 let arrownext=document.querySelector(".next")
+let mais=document.querySelector(".mais")
+let menos=document.querySelector(".menos")
+let qtdproduto=document.querySelector(".qtdproduto")
+let comprado=document.querySelector(".comprado")
+let carrinho=document.querySelector(".carrinho")
+let descricaoCompra=document.querySelector(".descricaoCompra")
+let qntidadecompradaHTML=document.querySelector(".qntidadecompradaHTML")
+let totalcompra=document.querySelector(".totalcompra")
+let vazio=document.querySelector(".vazio")
+
 
 
 let images=["image-product-1.jpg",`image-product-2.jpg`,`image-product-3.jpg`,`image-product-4.jpg`]
@@ -30,17 +41,62 @@ arrownext.addEventListener("click",()=>{
     imgatual.setAttribute('src', "images/image-product-"+[i]+".jpg")
 
 })
+//voltando foto//
+arrowbaack.addEventListener("click",()=>{
+    i--
+    if(i<1){
+        i=4
+    }
+    imgatual.setAttribute('src', "images/image-product-"+[i]+".jpg")
+
+})
 
 
 
 
 
+//adicionar quantidade/
+let quantidade=0
+mais.addEventListener("click",()=>{
+    quantidade++
+    qtdproduto.innerHTML=quantidade
+    Compras()
+})
+
+menos.addEventListener("click",()=>{
+    quantidade--
+    if(quantidade<0){
+        quantidade=0
+
+    }
+    qtdproduto.innerHTML=quantidade
+    Compras()
+})
 
 
 
 
+//clicando no carrinho mostra quantos foram comprados//
 
+carrinho.addEventListener("click",()=>{
 
+    comprado.style.display="block"
+    paiarow.style.display="none"
+})
+// caso não tenha comprado nada e clicar no carrinho//
+    function Compras(){
+        if(quantidade > 0){
+            vazio.style.display="none"
+            descricaoCompra.style.display="block"
+            qntidadecompradaHTML.innerHTML=quantidade
+            totalcompra.innerHTML="$"+125.00*quantidade
+        }
+        else{
+            descricaoCompra.style.display="none"
+            vazio.style.display="block"
+        }
+
+    }
 
 
 
